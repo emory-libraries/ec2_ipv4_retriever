6b8f62c1ab1d7167b2616d006afd8ab1ed31c15e
bwatson78 <brad.watson.orlando@gmail.com>
2025-04-15 09:58:33 -0500

    First Commit.

---
+++ lib/ec2_ipv4_retriever.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative "ec2_ipv4_retriever/version"
+require 'aws-sdk-ec2'
+
+module Ec2Ipv4Retriever
+  class Error < StandardError; end
+
+  def find_ip_by_ec2_name(region: 'us-east-1', ec2_name:)
+    ec2_resource = ::Aws::EC2::Resource.new(region:)
+    ec2_instance = pull_ec2_instance_by_tag_name(ec2_resource:, ec2_name:)
+
+    ec2_instance.nil? ? nil : ec2_instance.private_ip_address
+  end
+
+  private
+
+  def pull_ec2_instance_by_tag_name(ec2_resource:, ec2_name:)
+    ec2_resource&.instances(filters: [{ name: "tag:Name", values: [ec2_name] }])&.first
+  end
+end
---
+++ lib/ec2_ipv4_retriever/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Ec2Ipv4Retriever
+  VERSION = "0.1.0"
+end
